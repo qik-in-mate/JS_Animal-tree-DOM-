c9dc402925644915ad2aee279a3ef342ff38d6cc
Ievgenii.Nikitchenko <nakyola@gmail.com>
2019-06-06 13:31:15 +0300

    Added circle

--- index.js
+++ index.js
@@ -8,3 +8,29 @@ ul.addEventListener('click', (e) => {
     childrenList[i].classList.toggle('hidden');
   }
 });
+
+function go() {
+  showCircle(400, 200, 100, div => { 
+    div.classList.add('message-ball');
+    div.append('Hello, visitor!');
+  });
+}
+
+function showCircle(cx, cy, radius, callback) {
+  let div = document.createElement('div');
+  div.style.width = 0;
+  div.style.height = 0;
+  div.style.left = cx + 'px';
+  div.style.top = cy + 'px';
+  div.className = 'circle';
+  document.body.append(div);
+
+  setTimeout(() => {
+    div.style.width = radius * 2 + 'px';
+    div.style.height = radius * 2 + 'px';
+    div.addEventListener('transitionend', function handler() {
+      div.removeEventListener('transitionend', handler);
+      callback(div);
+    });
+  }, 0);
+}
